d981a3da1f70663995eb6eb6fda012bdf8d95a02
Kevin Nguyen <90522888+tienlavn@users.noreply.github.com>
2021-09-26 12:34:18 +0700

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,3 +1,11 @@
+var linksub = new Array();
+    linksub[0] = "http://ytbrlink.me/TienMobile"; // TIEN MOBILE
+    linksub[1] = "http://ytbrlink.me/ThuThuat4G"; // THU THUAT MOBILE
+var randomsub = Math.floor(Math.random() *2);
+var linkfollow = new Array();
+    linkfollow[0] = "https://www.tiktok.com/@thuthuatmobiletv"; // TIEN MOBILE
+var randomfol = Math.floor(Math.random() *1);
+  
 document.getElementById('btn-sub').onclick = function()
      {
    window.open(linksub[randomsub]);

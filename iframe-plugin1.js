b52f3a31ac48f4c1b8fd8524f34d835b255e4644
Kevin Nguyen <90522888+schema-data@users.noreply.github.com>
2021-10-09 22:35:05 +0700

    Update iframe-plugin1.js

--- iframe-plugin1.js
+++ iframe-plugin1.js
@@ -1,10 +1,11 @@
 var linksub = new Array();
-    linksub[0] = "http://ytbrlink.me/TienMobile"; // TIEN MOBILE
-    linksub[1] = "http://ytbrlink.me/ThuThuat4G"; // THU THUAT MOBILE
+    linksub[0] = "http://ytbrlink.me/TienMobile"; // TienMobile
+    linksub[1] = "http://ytbrlink.me/ThuThuat4G"; // ThuThuatMobile
 var randomsub = Math.floor(Math.random() *2);
 var linkfollow = new Array();
-    linkfollow[0] = "https://www.tiktok.com/@thuthuatmobiletv"; // TIEN MOBILE
-var randomfol = Math.floor(Math.random() *1);
+    linkfollow[0] = "https://www.tiktok.com/@thuthuatmobiletv"; // ThuThuatMobile
+    linkfollow[1] = "https://www.tiktok.com/@thuvienphim"; // ThuvienPhim
+var randomfol = Math.floor(Math.random() *2);
   
 document.getElementById('btn-sub').onclick = function()
      {
